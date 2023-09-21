29af40181ab8536062971d768d405da6a72c5d5a
AveragePythonUser <109878558+AveragePythonUser@users.noreply.github.com>
2023-09-20 19:07:44 -0700

    can (kind of) grab objects now
    needs more work but objects can follow player

--- Assets/Scripts/Player/Detection.cs
+++ Assets/Scripts/Player/Detection.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using TMPro.EditorUtilities;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -8,21 +10,50 @@ using UnityEngine.EventSystems;
 public class Detection : MonoBehaviour
 {
 
+    [Header("Binds")]
+    public KeyCode Key_E = KeyCode.E;
+
+    [Header("Other")]
     public Transform player_camera;
     [SerializeField]
     private int select_distance;
     public LayerMask layer;
 
+    // Objects and Components
     private HUD_manager hud_manager;
+    private Transform orientation;
+
+    private bool holding;
+    private Transform holding_transform;
     
     // Start is called before the first frame update
     void Start()
     {
-        hud_manager = GetComponent<HUD_manager>();
+        hud_manager = GameObject.Find("HUD").GetComponent<HUD_manager>();
+        orientation = GameObject.Find("Orientation").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if (holding == true)
+        {
+            holding_object();
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
+    {
+        select_check();
+    }
+
+    private void holding_object()
+    {
+        holding_transform.position = gameObject.transform.position + new Vector3(3, 0, 0);
+        //holding_transform.position = player_camera.transform.forward;
+    }
+
+    private void select_check()
     {
         Ray ray = new Ray(player_camera.position, player_camera.forward);
         RaycastHit hit;
@@ -32,21 +63,23 @@ public class Detection : MonoBehaviour
             if (hit.transform.gameObject.CompareTag("Selectable"))
             {
                 hud_manager.Press_E(true);
-                Selected();
+                Selected(hit);
             }
-            else
-            {
-                hud_manager.Press_E(false);
-            }
-            
-            Debug.Log("collide");
+        }
+        else
+        {
+            hud_manager.Press_E(false);
         }
         Debug.DrawRay(player_camera.position, player_camera.forward * select_distance, Color.yellow);
-
     }
 
-    private void Selected()
+    private void Selected(RaycastHit hit)
     {
-        Debug.Log("selected");
+        if (Input.GetKey(Key_E))
+        {
+            holding = true;
+            holding_transform = hit.transform;
+        }
+
     }
 }
